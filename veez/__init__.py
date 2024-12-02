719062326261dafb6602d1ff4a469fefe7e312f8
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2024-12-02 19:13:09 +0530

    Update __init__.py

--- veez/__init__.py
+++ veez/__init__.py
@@ -1,1 +1,31 @@
+#pyrogram imports
+from pyrogram import Client
+from pyrogram import idle
 
+#py-tgcalls imports 
+from pytgcalls.types import GroupCallConfig 
+from pytgcalls import PyTgCalls
+
+#manual imports 
+import asyncio 
+import os
+
+veez = Client(
+     "veezmusic", 
+     api_id=API_ID, 
+     api_hash=API_HASH, 
+     bot_token=BOT_TOKEN, 
+     plugins=dict(root="veez/modules")
+) 
+
+veez_user = Client(
+          ":memory:", 
+          api_id=API_ID, 
+          api_hash=API_HASH, 
+          session_string=str(SESSION), 
+          in_memory=True, 
+) 
+call_py = PyTgCalls(veez_user)
+config = GroupCallConfig(auto_start=False)
+
+       
