f477b6a6cf15ec7ec955f44b987543191c72278f
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2024-12-02 22:44:10 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-DATA [
+DATA = [
     "The only way to do great work is to love what you do. – Steve Jobs",
     "Success is not the key to happiness. Happiness is the key to success. – Albert Schweitzer",
     "Believe you can and you're halfway there. – Theodore Roosevelt",
