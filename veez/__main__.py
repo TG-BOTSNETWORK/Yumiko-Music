e5554e76d29147fe81207398e872cedd1262139d
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2024-12-02 19:22:49 +0530

    Update __main__.py

--- veez/__main__.py
+++ veez/__main__.py
@@ -1,1 +1,11 @@
+import asyncio
 
+#module imports
+from veez import veez 
+from veez import veez_user 
+from veez import call_py 
+from veez import LOGGER
+#pyrogram imports 
+from pyrogram import idle 
+#py-tgcalls imports 
+from pytgcalls import idle as veez_idle
