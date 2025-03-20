dff73eb066da838c8ee6b41ad4714877443ec941
TG-BOTSNETWORK <151555579+TG-BOTSNETWORK@users.noreply.github.com>
2025-03-20 20:28:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,26 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# Configuration
 API_ID = os.getenv("API_ID", "27805819")
 API_HASH = os.getenv("API_HASH", "7372a27cd4dc20b792bb117b038db7ef")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7481943486:AAEWhDdfqQEE9SA3vBQb4MZ_MsM8p3hgSVc")
 SESSION = os.getenv("SESSION", "BQGoSHsAt-kneFsPDgFBXUjxim7d8PT6nMfQZUATeOuSXRwwL1o43z63EytXmVINC_xnEIcMP1wz8G5PNQvJQGl8iSj8UC9KRrTnNaQyD7fb7G381ft3_2hBVAgioO4l8ollcltDtCJRFBsFhUN5dOPwfVA8WNCQ2OooVZPa9n_segouxGxLB7p51PdwQ0ewu94P2COz0OZQ5Ieu3bev0nKQ9iPuAxjaO4ryTTYkVmJQF4tgSJ3amoVY9ad3ldteq1tAUgdK5HJqVhDO8xX7ngxEHhjjVKgDIrQyt8LuX4hKaCve2izbIsid2pcuMePPpeB92wHte2ZzTzv1yeiH_AwX_7G-bQAAAAG9QfXgAA")
-BOT_USERNAME = os.getenv("BOT_USERNAME", "TgGroupMusicBot") 
+BOT_USERNAME = os.getenv("BOT_USERNAME", "TgGroupMusicBot")
+YOUTUBE_COOKIES = os.getenv("YOUTUBE_COOKIES", "cookies.txt")
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "abfefd79368745e980d5ebb3dafe668d")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "c03fa79f2fdb4ca0933ea4fad7697160")
+
 # Ensure all required variables are set
-required_vars = ["API_ID", "API_HASH", "BOT_TOKEN", "SESSION"]
+required_vars = [
+    "API_ID", 
+    "API_HASH", 
+    "BOT_TOKEN", 
+    "SESSION",
+    "YOUTUBE_COOKIES",
+    "SPOTIFY_CLIENT_ID",
+    "SPOTIFY_CLIENT_SECRET"
+]
 missing_vars = [var for var in required_vars if not globals()[var]]
 
 if missing_vars:
